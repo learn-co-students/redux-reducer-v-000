48c616f651f4816546058c13088e6137a5be0bae
Matthew <pickledyamsman@gmail.com>
2017-02-23 11:29:11 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+  if (action.type == "INCREASE") {
+    return Object.assign({}, state, {
+      numberOfPresents: state.numberOfPresents + 1
+    })
+  } else {
+    return state
+  }
 }
