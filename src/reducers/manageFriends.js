fdce733a26333a3cd80e3c59ef737e900edef0b0
Cernan Bernardo <cernanb@gmail.com>
2017-03-06 20:28:57 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  // console.log(action)
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      const newState = Object.assign({}, state, {friends: [...state.friends, action.friend]})
+      return newState
+    case 'REMOVE_FRIEND':
+      return Object.assign({}, state, {friends: state.friends.filter(friend => friend.id !== action.id)})
+    default:
+      return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+  
+  switch(action.type) {
+    case 'INCREASE':
+      // const newState = Object.assign({}, )
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state
+  }
 }
