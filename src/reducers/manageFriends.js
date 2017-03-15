4e6e9caf4711731de2c0eaea8c784a74ef98d2d6
Adam <lucia.adam@gmail.com>
2017-03-15 13:59:09 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+  if (action.type === "ADD_FRIEND") {
+    const addFriend = action.friend
+    const newFriends = [...state.friends, addFriend];
+    return {friends: newFriends};
+  } else {
+    if (action.type === "REMOVE_FRIEND") {
+      const removeIndex = state.friends.findIndex(friend => friend.id === action.id)
+      const newFriends = [
+        ...state.friends.slice(0, removeIndex),
+        ...state.friends.slice(removeIndex + 1)
+
+      ]
+      return {friends: newFriends};
+    } else {
+      return state;
+    }
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,16 @@
 export function managePresents(state, action){
+  if (!state) {
+    const state = {
+      numberOfPresents: 0
+    }
+  }
+  if (action.type === "INCREASE") {
+    const newState = Object.assign({}, state, {
+      numberOfPresents: state.numberOfPresents + 1
+    });
+
+    return newState;
+  } else {
+    return state;
+  }
 }
