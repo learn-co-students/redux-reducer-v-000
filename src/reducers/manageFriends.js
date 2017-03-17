265570fa14b68212deabb7a63b21dd3461d8e982
Yechiel K <chilikkal@gmail.com>
2017-03-16 21:28:21 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND') {
+
+    return {...state, friends: [...state.friends, action.friend]}
+
+  } else if (action.type === 'REMOVE_FRIEND') {
+
+    return {...state, friends: state.friends.filter(friend => {
+      return friend.id !== action.id
+    })}
+
+  }
+  return state
 }
