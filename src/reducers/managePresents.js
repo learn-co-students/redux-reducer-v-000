59f40dcbbe937fa4e57182c6d026fe55a2688b2a
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-04-06 23:48:29 +0000

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
-export function managePresents(state, action){
-}
+export function managePresents(state = { numberOfPresents: 0 }, action){
+  switch(action.type) {
+
+    case 'INCREASE':
+      const newNumberOfPresents = state.numberOfPresents + 1
+      return Object.assign({}, state, { numberOfPresents: newNumberOfPresents });
+
+    default:
+      return state;
+  }
+};
