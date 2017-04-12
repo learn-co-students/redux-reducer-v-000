f8670b277bc9b9fbfad9651c2592d90de77107dc
msosborne3 <msosborne@ymail.com>
2017-04-11 22:50:29 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+
+  if(action.type == "ADD_FRIEND") {
+    return Object.assign({}, state, {
+      friends: [...state.friends, action.friend]
+    });
+  } else if(action.type == "REMOVE_FRIEND") {
+    const removeIndex = state.friends.findIndex(friend => friend.id == action.id);
+    return Object.assign({}, state, {
+      friends: [...state.friends.slice(0, removeIndex),
+      ...state.friends.slice(removeIndex + 1)]
+    });
+  } else {
+    return state;
+  }
 }
