484cd0e9585a472c40660b04ab84e39ea5cfe77c
Christian Whitesides <whitesidescr@gmail.com>
2017-05-29 13:57:53 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const friendArray = state.friends.slice()
+      const indexOfFriendToBeRemoved = friendArray.findIndex(friend => friend.id === action.id)
+      friendArray.splice(indexOfFriendToBeRemoved, 1)
+      return { friends: friendArray }
+    default:
+      return state
+  }
 }
