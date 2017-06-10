dd2c75e6ed0340a3846e8ffe7ab8905ca73f02e0
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-10 11:59:29 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+    let friends = state.friends.slice();
+    let friend = Object.assign({},action.friend);
+    switch (action.type) {
+        case "ADD_FRIEND": 
+            friends = [...friends, friend];
+            return {...state, friends: friends};
+        case "REMOVE_FRIEND":
+            let indexToRemove = friends.findIndex(function(f){
+                return f.id === action.id;
+            })
+            friends.splice(indexToRemove,1);
+            return {...state, friends: friends}
+        default:
+            return state;
+    }
 }
