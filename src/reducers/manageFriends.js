4fbe930cefa7746408b0897c9897ed41aab7025f
Jesse L Lawson <homestarflyer@gmail.com>
2017-06-18 17:51:54 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,31 @@
 export function manageFriends(state, action){
+
+  let friendManager;
+
+  switch (action.type) {
+
+    case 'ADD_FRIEND':
+
+      friendManager = Object.assign(
+        ...state,
+        {friends: [...state.friends, action.friend]}
+      )
+
+      break;
+
+    case 'REMOVE_FRIEND':
+
+      let r = state.friends.findIndex(f => f.id === action.id)
+
+      friendManager = Object.assign(
+        ...state, {friends: [...state.friends.slice(0, r),
+        ...state.friends.slice(r + 1)]}
+      )
+
+      break;
+
+    default:friendManager = state
+
+  }
+  return friendManager
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+  let presentUpdate;
+  switch (action.type) {
+    case 'INCREASE':
+      presentUpdate = Object.assign(...state, { numberOfPresents: state.numberOfPresents + 1 })
+      break;
+    default: presentUpdate = state
+
+  }
+  return presentUpdate
 }
