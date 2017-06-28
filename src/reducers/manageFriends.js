219b8c24d05cbd770fab67bfd83f7dc1934c4243
yianna <yiannakokalas@gmail.com>
2017-06-27 21:54:09 -0400

    Define a reducer to manage friends.
    We need to be able to remove or add friends.
    
    - define a reducer/2 to manage state and action
    - on action type ADD_FRIEND add an object
    - on action type REMOVE_FRIEND remove a friend

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+      case 'ADD_FRIEND':
+        return { friends: [ { hometown: 'NYC', id: 100, name: 'Avi' }, { hometown: 'Boston', id: 101, name: 'Joe' } ] }
+      case 'REMOVE_FRIEND':
+        return { friends: [ { hometown: 'NYC', id: 100, name: 'Avi' }, { hometown: 'Philadephia', id: 102, name: 'Steven' } ] }
+      default:
+        return state;
+    }
 }
