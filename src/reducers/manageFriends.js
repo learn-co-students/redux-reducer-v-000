dd09bf183267ca2c0c4320382e769768760a046f
Zenia <zeniavilla@hotmail.com>
2017-08-01 14:20:37 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]}
+        case 'REMOVE_FRIEND':
+            return {friends: state.friends.filter(function (obj) { return obj.id !== action.id })}
+        default:
+            return state;
+    }
 }
