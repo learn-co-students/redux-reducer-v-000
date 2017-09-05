89959653a5b23e9dd7b831a44b53db97dab306a6
Howard DeVennish <howarddevennish@gmail.com>
2017-09-05 09:42:51 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,12 +1,14 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      let addFriendArr = state.friends;
-      addFriendArr.push(action.friend);
-      return {friends: addFriendArr};
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends,
+          action.friend
+        ]
+      });
     case 'REMOVE_FRIEND':
-      let delFriendArr = state.friends;
-      let returnArr = delFriendArr.filter(friend => friend.id !== action.id);
+      let returnArr = state.friends.filter(friend => friend.id !== action.id);
       return {friends: returnArr};
     default: return state;
   }
