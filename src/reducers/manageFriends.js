64df1fe6b86f4e32d6b56455952694a88474d2c6
Jschles1 <jschles1@ramapo.edu>
2017-09-08 16:51:12 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case'ADD_FRIEND':
+      return {
+        ...state,
+        friends: [...state.friends, action.friend]
+      };
+    case 'REMOVE_FRIEND':
+      return {
+        ...state,
+        friends: state.friends.filter(friend => action.id !== friend.id)
+      };
+    default:
+      return state;
+  }
 }
