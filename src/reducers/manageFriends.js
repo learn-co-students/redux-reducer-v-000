4b8b1e11733d7a7ef3c1241f8d0222b9255a7d0b
John Pfingst <jpfingst@outlook.com>
2017-09-17 20:34:00 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]};
+    case 'REMOVE_FRIEND':
+      const idx = state.friends.findIndex(e => e.id === action.id);
+      if (idx !== -1) {
+        return {friends: [...state.friends.slice(0,idx),...state.friends.slice(idx+1)]};
+      }
+      // else fall through to default.
+    default:
+      return state;
+  }
 }
