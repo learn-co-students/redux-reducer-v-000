9af0b875ca29ecec8c07f58e2409a4944288681d
curlywallst <nnoyes@comcast.net>
2017-10-25 18:06:49 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch(action.type) {
     case 'ADD_FRIEND':
-      return {friends: state.friends.concat(action.friend)}
+      return Object.assign({}, state, {friends: state.friends.concat(action.friend)})
     case 'REMOVE_FRIEND':
       return Object.assign({}, state, {friends: state.friends.filter(function(f) {return f.id !== action.id})})
     default:
