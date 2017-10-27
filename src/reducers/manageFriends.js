978d8433c3e72473dfd2a7b7bd61d52d7bc31e62
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-10-27 15:31:42 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+  console.log(state.friends);
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return Object.assign({}, state, {
+        friends: [...state.friends, action.friend]
+      })
+    case 'REMOVE_FRIEND':
+        return {friends: state.friends.filter(item => item.id !== action.id)}
+    default:
+      return state;
+
+  }
 }
+
+/////////
+
+// Object.assign({}, state, {
+//   friends: [...state.friends, action.friend]
+//
+// });
