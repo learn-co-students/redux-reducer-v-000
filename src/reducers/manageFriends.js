bf255748b050e4f29508c235558acb022e0569e4
Bryan Hou <bryan.hou94@gmail.com>
2017-11-06 22:58:04 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+	switch (action.type) {
+		case 'REMOVE_FRIEND':
+			return { friends: state.friends.filter((friend) => friend.id !== action.id)};
+		case 'ADD_FRIEND':
+			return { friends: [...state.friends, action.friend]};
+		default:
+			return state;
+	}
 }
