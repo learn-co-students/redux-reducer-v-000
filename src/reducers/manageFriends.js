48e4d7a58475ab8a600dc5a8745791fc627cf426
smithwebtek <brad@smithwebtek.com>
2017-11-13 17:38:30 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
 export function manageFriends(state, action){
-}
+  switch (action.type){
+
+    case 'Random Action Type': 
+        !Object.keys(state).includes(action.type)
+      return state
+
+    case "ADD_FRIEND":
+    return Object.assign({}, state, {
+      friends: [
+        ...state.friends,
+        action.friend
+      ]
+    });
+    
+    case 'REMOVE_FRIEND':
+      const unfriend_id = state.friends.findIndex(friend => friend.id === action.id)
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends.slice(0, unfriend_id), 
+          ...state.friends.slice(unfriend_id + 1)
+        ]
+      });
+   
+
+    default:
+        return state
+    }
+  }
+
