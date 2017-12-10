be5c338a63b783fc4cfef68b2630a5dc60c6af44
sukey <sdklos@gmail.com>
2017-12-10 15:06:11 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND' && 'name' in action.friend && 'hometown' in action.friend && 'id' in action.friend) {
+    return Object.assign({}, state, {friends: [...state.friends, action.friend] })
+  } else if (action.type === 'REMOVE_FRIEND' && action.id){
+    const formerFriend = state.friends.find(f => f.id === action.id)
+    const formerFriendIndex = state.friends.indexOf(formerFriend)
+    return Object.assign({}, state, {friends: [...state.friends.slice(0,formerFriendIndex), ...state.friends.slice(formerFriendIndex + 1)]})
+  } else {
+    return state
+  }
 }
