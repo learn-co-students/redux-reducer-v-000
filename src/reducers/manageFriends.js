39a2192577156f289cee1884bca567ea8c34d2a2
ruuuh20 <pkim2320@gmail.com>
2017-12-15 19:38:02 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+    return {friends: [
+      ...state.friends, action.friend
+    ]};
+    case 'REMOVE_FRIEND':
+    // const removeFriend = state.friends.findIndex(friend => friend.id === action.id);
+    // return {friends: [
+    //   ...state.friends.slice(0, removeFriend),
+    //   ...state.friends.slice(removeFriend + 1)
+    // ]}
+    return {friends: state.friends.filter( friend => {
+      return friend.id !== action.id
+    })}
+    default:
+    return state;
+  }
 }
