98e44ecddb0d4139afbbf69441bbcec5195c39e2
Bruce Molina <bruce.molina.81@gmail.com>
2017-12-19 03:02:54 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  console.log(action)
+  switch(action.type){
+    case 'ADD_FRIEND':
+      const friend = action.friend
+      return {...state, friend}
+    case 'REMOVE_FRIEND':
+      return {}
+    default:
+    return state;
+  }
 }
+// friends: [ { name: 'Avi', hometown: 'NYC', id: 100 } ] }
