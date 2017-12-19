0443705df06268d5fb7b8edadda6e646b0a4c96e
Sophia Hubeny <shhubeny@gmail.com>
2017-12-19 04:41:38 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return Object.assign({}, state, {
+        friends: [
+        ...state.friends,
+        action.friend
+      ]})
+    case 'REMOVE_FRIEND':
+      let removalIndex = state.friends.findIndex(friend => friend.id === action.id) //take the action.id that was passed into the function and look in the array of state.friends for a match
+      return Object.assign({}, state, {
+        friends: [ //since slice is not destructive, hash spread in the array
+          ...state.friends.slice(0,removalIndex), //gets all friends to the left of removalIndex
+          ...state.friends.slice(removalIndex+1) //gets all friends to right of removalIndex
+        ]})
+    default:
+      return state
+  }
 }
