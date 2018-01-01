b3cc6fad5b7e6e073e5f1ed38e1dcad403825093
NickisKnowledge <lifechangingstuff@gmail.com>
2017-12-31 20:12:03 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND': {
+      return Object.assign(
+        {}, state,
+        { friends: [...state.friends, action.friend] }
+      );
+    }
+
+    case 'REMOVE_FRIEND': {
+      const remainingFriends = state.friends.filter(friend => {
+        return friend.id !== action.id;
+      });
+
+      return Object.assign(
+        {}, state,
+        { friends: remainingFriends }
+      );
+    }
+
+    default: {
+      return state;
+    }
+  }
 }
