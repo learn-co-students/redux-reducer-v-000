b15fe249dc4ffe3ec12747060149e1f14a259f6d
gb23 <gregbenj@hotmail.com>
2018-01-03 13:33:24 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    let friendsArray = [];
+    switch (action.type) {
+        case "ADD_FRIEND":
+            friendsArray = [...state.friends, action.friend];
+            return {...state, friends: friendsArray};
+        case "REMOVE_FRIEND":
+            friendsArray = state.friends.filter(friend => friend.id !== action.id); 
+            return {...state, friends: friendsArray};
+        default:
+            return state;
+    }
 }
