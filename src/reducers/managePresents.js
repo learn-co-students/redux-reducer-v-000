3d01c23568b4c933e1cc2ece5ff20274c9ea6768
Monika Williams <monika.m.williams@gmail.com>
2018-01-05 14:22:16 +0000

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+    return Object.assign({}, state, {
+      numberOfPresents: state.numberOfPresents + 1
+    });
+    default:
+      return state;
+  }
 }
+
+let state = {numberOfPresents: 0}
+let action = {type: 'INCREASE'}
