b42545b0877a0fa56b81b957d66ac90b535c50a1
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-26 13:37:09 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+     return Object.assign({}, state, {
+       friends: [
+         ...state.friends,
+         action.friend
+       ]
+     });
+     case 'REMOVE_FRIEND':
+      const filteredFriends = state.friends.filter(f => f.id !== action.id);
+      return Object.assign({}, state, {
+          friends: [
+            ...filteredFriends
+          ]
+        });
+    default:
+      return state;
+  }
 }
