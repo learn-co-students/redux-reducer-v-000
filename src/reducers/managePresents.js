3aab697de91750f88d850f7dd71b317e3acdc6cf
Andrew <andrew.ramell@gmail.com>
2018-01-27 17:05:38 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
 export function managePresents(state, action){
 }
+// let action = {type: 'Random_Action_Type'}
+// let state = {number: 0}
+function managePresents(state, action) {
+  
+  switch (action.type){
+    case 'INCREASE':
+      return Object.assign({}, state, {numberOfPresents: state.numberOfPresents + 1})
+      default:
+        return state
+  
+  }
+
+}
