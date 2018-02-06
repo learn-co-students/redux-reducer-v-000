ce6f66e5670ea8a36ea60ab331598f7d56aa9a3a
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-06 03:00:49 -0800

    levearged ES6 notation for object cloning

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,15 +1,14 @@
-export function manageFriends(state, action){
-  const stateClone = {...state};
+export function manageFriends(state = { friends: [] }, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      stateClone.friends.push(action.friend);
-      return stateClone;
+      return {...state, friends: [...state.friends, action.friend] }
+      //clone the state object, then overwrite the state's key friends with the second argument
     case 'REMOVE_FRIEND':
-      const filteredFriends = stateClone.friends.filter(function(friend){
-        return friend.id !== action.id;
-      })
-      return {friends: filteredFriends};
+      const filteredFriends = state.friends.filter((friend) =>
+        friend.id !== action.id
+      )
+      return {...state, friends: filteredFriends}
     default:
-      return stateClone;
+      return {...state};
   }
 }
