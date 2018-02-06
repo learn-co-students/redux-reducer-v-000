31409dab13968588d18b8d261ffb7fd173a1f696
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-06 02:41:59 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  const stateClone = {...state};
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      stateClone.friends.push(action.friend);
+      return stateClone;
+    case 'REMOVE_FRIEND':
+      const filteredFriends = stateClone.friends.filter(function(friend){
+        return friend.id !== action.id;
+      })
+      return {friends: filteredFriends};
+    default:
+      return stateClone;
+  }
 }
