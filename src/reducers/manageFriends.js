963d8f9eda0b8d96feeab9588db6388c8af38fc7
Janusz Szubert <theinvalidnonce@gmail.com>
2018-03-20 17:31:41 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      let friendList = [...state.friends, action.friend]
+      return { friends: friendList }
+    case 'REMOVE_FRIEND':
+      let matchingFriend = state.friends.filter(
+        friend => friend.id !== action.id)
+        
+      return { friends: matchingFriend}
+    default: 
+      return state
+  }
 }
