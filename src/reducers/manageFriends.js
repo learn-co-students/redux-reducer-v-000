22d3c79d469552def716d7579a44027951bb38b9
Alexander Urizar <urizaraj@gmail.com>
2018-04-16 12:13:28 -0400

    complete lab

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case ('ADD_FRIEND'):
+      const friend = action.friend
+      const friends = [...state.friends, friend]
+      return {...state, friends}
+    case ('REMOVE_FRIEND'):
+      const id = action.id
+      const res = state.friends.filter(f => f.id !== id)
+      return {...state, friends: res}
+    default: return state
+  }
 }
