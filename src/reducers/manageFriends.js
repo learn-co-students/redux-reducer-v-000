bf60d38805c0d931c08bf4ec94c8e73092efb1c1
David Towster <djt4xe@virginia.edu>
2018-04-25 19:05:03 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+  console.log(action);
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]};
+    case 'REMOVE_FRIEND':
+      let removeIndex = state.friends.findIndex((friend) => {
+        return friend.id === action.id;
+      });
+      
+      return {friends: [
+        ...state.friends.slice(0, removeIndex), 
+        ...state.friends.slice(removeIndex+1, state.friends.length)
+      ]};
+    default: 
+      return state;
+  }
 }
