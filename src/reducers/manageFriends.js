49f27992c1dc9aace1e22c25434830e91f8fcd7f
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-28 19:54:55 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        
+        case 'ADD_FRIEND':
+            return {...state, friends: [...state.friends, {'hometown': action.friend.hometown, 'id': action.friend.id, 'name': action.friend.name }]};
+        case 'REMOVE_FRIEND':
+            let index = state.friends.findIndex((element) => element.id === action.id)
+            return {state.friends: state.friends.splice(index,1)}
+        default: return state;
+    }
+    
 }
