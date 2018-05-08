a1ff97cf71d18b13e1eb81d7a68c002f5bb09ef5
Sophia Varella <sophiavarella@gmail.com>
2018-05-07 19:19:26 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]}
+    case 'REMOVE_FRIEND':
+      return {...state, friends: state.friends.filter(x => x.id != action.id)}
+    default:
+      return state;
+  }
 }
