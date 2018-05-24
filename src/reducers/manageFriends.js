1f0eddb7274f869cf023fd6f2a68835038de8682
Adjoa <adjoa.darien@gmail.com>
2018-05-24 23:59:32 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return {
+        friends: state.friends.concat(action.friend)
+      }
+    case 'REMOVE_FRIEND':
+      const index = state.friends.indexOf(action.friend)
+      return {
+        friends: state.friends.slice(0, index - 1).concat(state.friends.slice(index))
+      }
+    default:
+      return state;
+  }
 }
