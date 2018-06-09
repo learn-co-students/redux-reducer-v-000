a3a705fd01e22e95f87abfeb8135ca0468f846b4
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-09 14:35:49 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,16 +1,28 @@
 export function manageFriends(state, action){
   switch (action.type) {
-    case 'ADD_FRIEND':
-      return {friends: [...state.friend, action.friend]}
-    case 'REMOVE_FRIEND':
-      const id = action.id
-      let friendCopy = []
-        for (x of action.friends) {
-          if (x.id !== id) {
-            friendCopy << x
-          }
-        }
-      return {friends: friendCopy}
+
+    case "ADD_FRIEND":
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends,
+          action.friend
+        ]
+      });
+
+      case "REMOVE_FRIEND":
+          // finds the index in the array of the element that satisfies the condition:
+        const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+          // then create a new object, add state, and merge the rest in
+        return Object.assign({}, state, {
+          friends: [
+              // take from friends array everything up to the element to be removed
+            ...state.friends.slice(0, removalIndex),
+              // and add to friends array everything after the element to be removed,
+              // so you skip that whcih is to be removed
+            ...state.friends.slice(removalIndex + 1)
+          ]
+        });
+
     default:
       return state
   }
