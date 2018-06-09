1c913f2c22cc6fe6bdc986e9d5f3ead1d163e194
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-09 14:25:27 -0400

    manageFriends not working

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friend, action.friend]}
+    case 'REMOVE_FRIEND':
+      const id = action.id
+      let friendCopy = []
+        for (x of action.friends) {
+          if (x.id !== id) {
+            friendCopy << x
+          }
+        }
+      return {friends: friendCopy}
+    default:
+      return state
+  }
 }
