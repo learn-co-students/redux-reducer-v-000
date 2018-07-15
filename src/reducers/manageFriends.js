2c2e17a1032a5d6b6c254c8da4462d6f656baa3e
lawrend <douglas_lawrence@mac.com>
2018-07-14 23:56:51 -0700

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      const newFriend = {'name': action.friend.name, 'hometown': action.friend.hometown, 'id': action.friend.id}
+      return {friends: (state.friends).concat(newFriend)}
+    case 'REMOVE_FRIEND':
+      if(action.hasOwnProperty('id')){
+        const newList = (state.friends).filter(function(el) {
+          return el.id !== action.id;
+        });
+        return {friends: newList}
+      }
+    default:
+      console.log('default called')
+      return state
+  }
+
 }
