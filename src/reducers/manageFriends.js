e747e109d1c5125d5afc49be4b0f634a58b9216b
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-07-18 23:13:51 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        // using Object.assign, not avail in ES6
+        // case 'ADD_FRIEND':
+        //     return Object.assign({}, state, {
+        //         friends: [
+        //             ...state.friends,
+        //             action.friend
+        //         ]
+        //     });
+
+        // case 'REMOVE_FRIEND':
+        // const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        // return Object.assign({}, state, {
+        //     friends: [
+        //         ...state.friends.slice(0, removalIndex),
+        //         ...state.friends.slice(removalIndex + 1)
+        //     ]
+        // });  
+
+        // using object spread operator
+        case 'ADD_FRIEND':
+            return {...state, friends: state.friends.concat(action.friend)}
+        case 'REMOVE_FRIEND':
+            return {...state, friends: state.friends.filter(friend => friend.id !== action.id)}
+
+        default:
+            return state;    
+    }
 }
