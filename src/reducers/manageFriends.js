0392f35cbdb9a51d6421a3b6dcf967fcc04e99e1
BuffaloRob <peckham.rob@gmail.com>
2018-07-20 10:28:09 -0400

    commented out and changed instances of Object.assign to spread operator

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,20 +1,35 @@
 export function manageFriends(state = { friends: [] }, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         friends: [
           ...state.friends,
           action.friend
         ]
-      });
+      }
+      // return Object.assign({}, state, {
+      //   friends: [
+      //     ...state.friends,
+      //     action.friend
+      //   ]
+      // });
     case 'REMOVE_FRIEND':
       const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
-      return Object.assign({}, state, {
+      return {
+        ...state,
         friends: [
           ...state.friends.slice(0, removalIndex),
           ...state.friends.slice(removalIndex + 1)
         ]
-      });
+      }
+      // return Object.assign({}, state, {
+      //   friends: [
+      //     ...state.friends.slice(0, removalIndex),
+      //     ...state.friends.slice(removalIndex + 1)
+      //   ]
+      // });
+        
     default:
       return state;
   }
