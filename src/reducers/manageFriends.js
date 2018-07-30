248235c74f6bc107ed49bdc9f7e222b2fee62558
Randall Robinson <keith_adams007@yahoo.com>
2018-07-30 18:29:40 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+    let newfriends = state.friends.push(action.friend)
+      return {friends: state.friends}
+    case 'REMOVE_FRIEND':
+    return {}
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberofPresents: state.numberofPresents}
+    default:
+      return state;
+  }
 }
