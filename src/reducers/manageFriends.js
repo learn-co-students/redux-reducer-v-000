f5d7d80a5159b541119e3389909139a2800ac12d
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 14:56:05 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const out = state.friends.findIndex(friend => friend.id == action.id);
+      return {friends: [...state.friends.slice(0, out), ...state.friends.slice(out + 1)]}
+    default:
+      return state;
+  }
 }
