b3ef16d449f1d221a1494582bf895b0e5aa266a8
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-08-01 16:23:54 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,9 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  if (action.type === 'ADD_FRIEND' && action.friend.name && action.friend.hometown && action.friend.id) {
+    return { friends: [...state.friends, action.friend] }
+  } else if (action.type === 'REMOVE_FRIEND' && action.id) {
+    return { friends: state.friends.filter(friend => friend.id !== action.id) }
+  } else {
+    return state
+  };
 }
