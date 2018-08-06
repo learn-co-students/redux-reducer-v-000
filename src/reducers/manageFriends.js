507de0f67ccb0c8bf902fecf06da60ec601e02c0
catalanowebdevelopment <catalano.work@gmail.com>
2018-08-06 19:57:16 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,22 @@
 export function manageFriends(state, action){
+   switch (action.type) {    
+       case 'ADD_FRIEND':
+           return {friends: [
+               ...state.friends, action.friend
+           ]}
+       case 'REMOVE_FRIEND':
+           return {friends: [
+               ...state.friends.filter(friend => friend.id !== action.id)
+           ]}
+       default:
+           return state
+   }
 }
+
+let state = { friends: [{
+    name: null,
+    hometown: null,
+    id: null
+}] }
+
+
