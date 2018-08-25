415978629a2d60cff795690924e9f6cb13cc5482
Aspen James <aspenjames@tqca.org>
2018-08-25 07:33:27 -0700

    add descriptive comments

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,8 +3,11 @@ export function manageFriends(state, action){
     case "ADD_FRIEND":
       return Object.assign({}, state, {friends: [...state.friends, action.friend]});
     case "REMOVE_FRIEND":
+      // deeply clone object
       let newState = JSON.parse(JSON.stringify(state));
+      // find index to remove
       let idx = newState.friends.findIndex(friend => friend.id === action.id);
+      // remove entry at idx and return the obj
       newState.friends.splice(idx, 1);
       return newState;
     default:
