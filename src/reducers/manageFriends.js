2fbad34c0a3183b36912693346bdfbba56e60e10
Aspen James <aspenjames@tqca.org>
2018-08-25 07:31:22 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return Object.assign({}, state, {friends: [...state.friends, action.friend]});
+    case "REMOVE_FRIEND":
+      let newState = JSON.parse(JSON.stringify(state));
+      let idx = newState.friends.findIndex(friend => friend.id === action.id);
+      newState.friends.splice(idx, 1);
+      return newState;
+    default:
+      return state;
+  }
+
 }
