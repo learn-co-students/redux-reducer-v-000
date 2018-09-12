0b64e58b7774619f2a226cc141061c67c097209e
badlychadly <chadmontoya21@yahoo.com>
2018-09-11 19:19:29 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,9 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {}
+    
+        default:
+            return state;
+    }
 }
