4b4dd8d017034445ac25ef445617a8583b781a11
Kiveo <mavericktesini@gmail.com>
2018-09-14 13:21:35 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+    return {...state, friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      let friends = state.friends.filter(friend => friend.id !== action.id ) 
+      return { friends }
+    default:
+    return {...state};
+  }
 }
+
+
+// manageFriends
+// removes the friend when action type is 'REMOVE_FRIEND' 
+// and the action has a property of the friends id to be removed:
