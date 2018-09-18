b8930f258b5cc7a389f93fe57fecbbb4d8b3ee2e
vanetoro <vanetoro84@gmail.com>
2018-09-18 12:26:04 -0700

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
 export function managePresents(state, action){
+    switch(action.type){
+        case 'INCREASE':
+          return {numberOfPresents: state.numberOfPresents + 1}
+        //  case: 'ADD_FRIEND':
+        //   return {friends: action.friend}
+        case 'REMOVE_FRIEND':
+         debugger
+         return {friends: state.friends.filter(friend => friend.id !== action.friend.id)}
+         default:
+         return state;
+    }
 }
