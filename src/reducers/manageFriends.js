8398a3c80259474dea95251e267fe117de69f37b
learn <eric.desimone@cortland.edu>
2018-09-18 07:27:11 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      {friends: state.friends.push(action.friend)}
+      return state;
+      case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+    default:
+      return state;
+  }
 }
