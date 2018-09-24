ad90b63d8f93807f4d7afe78c4e027faf87bca7f
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-09-24 16:18:00 -0700

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,22 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
+
+// action = {
+//   type: "ADD_FRIEND",
+//   friend: "Chrome Boi"
+// }
+
+// function changeState(state, action){
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     default:
+//       return state;
+//   }
+// }
