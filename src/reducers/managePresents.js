3bbad9b763b7906f5c332b51184ad20e55238024
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-30 11:15:55 -0700

    init commit

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
-export function managePresents(state, action){
+export function managePresents(state, action) {
+  if (action.type === 'INCREASE') {
+    return {
+      ...state,
+      numberOfPresents: state.numberOfPresents + 1
+    };
+  } else {
+    return state;
+  }
 }
