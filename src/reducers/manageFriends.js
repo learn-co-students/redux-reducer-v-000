f22d42944ce6283ea7f34a42d405ed542191c658
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-09 04:31:12 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,23 +1,28 @@
-export function manageFriends(state, action){
-  switch (action.type) {
-    case 'ADD_FRIEND':
-      return {
-        friends: [
-          ...state.friends,
-          {
-            name: action.friend.name,
-            hometown: action.friend.hometown,
-            id: action.friend.id
-          }
-        ]
-      }
-    case 'REMOVE_FRIEND':
-      let toDelete = new Set([action.id]);
-      let newArray = state.friends.filter(friend => !toDelete.has(friend.id));
-      return {
-        friends: newArray
-      }
+export function manageFriends(state = {
+  friends: [],
+}, action) {
+  switch(action.type) {
+
+    case "ADD_FRIEND":
+      return (
+        {...state,
+            action.friend
+        }
+    )
+
+    case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+
     default:
       return state;
+
   }
-}
+};
