e35fd8e79bd3e3b03ff6839415df98fdf85e45f4
Veronica Bustabad <vbustabad@gmail.com>
2018-10-20 22:30:34 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action){     
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const index = action.friend.id;
+      return {friends: [...state.friends.slice(101, index), ...state.friends.slice(index + 1)]}
+    default:
+      return state;
+  }
 }
