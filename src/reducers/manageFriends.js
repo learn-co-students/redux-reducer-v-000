30f7a96aedc25a44496648511b294c8b891a79bb
CSavi <carrielane89@gmail.com>
2018-10-31 11:02:49 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,34 @@
-export function manageFriends(state, action){
+
+let initialState = {
+    friends: []
+}
+
+let action = {
+    type: "ADD_FRIEND",
+    type: "REMOVE_FRIEND",
+    friend: "Chrome Boi"
+}
+
+export function manageFriends(state=initialState, action){
+    switch (action.type) {
+        case "ADD_FRIEND" :
+            return (
+                {...state, 
+                
+                friends: [...state.friends, action.friend]})
+        case "REMOVE_FRIEND" :
+            let removalIndex = state.friends.findIndex((friend) => friend.id === action.id);
+
+            return (
+                {...state,
+                
+                friends: [
+                    ...state.friends.slice(0, removalIndex), 
+                    ...state.friends.slice(removalIndex + 1)
+                ]
+                }
+            )
+        default :
+            return state;
+    }
 }
