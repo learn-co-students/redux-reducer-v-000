7177b66bc1be131fe43d4177460d65f775fa8134
tsyaeger <tsyaeger@gmail.com>
2018-11-23 19:29:26 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
 export function manageFriends(state, action){
+
+
+	let friend = action.friend
+
+	switch (action.type) {
+	case 'ADD_FRIEND':
+	  return  {friends: state.friends.concat(friend)}
+
+	case 'REMOVE_FRIEND':
+	  return {friends: state.friends.filter(friend => friend.id !== action.id)}
+
+	default:
+	  return state;
+	}
+
+
+
+
 }
+
+
+
+// let olderDog = Object.assign({}, dog, {age: dog.age + 1})
+// let olderDog = {...dog, age: dog.age + 1}
