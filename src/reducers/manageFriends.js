806d142d4c38d52b6139f600749d153869254b96
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-03 12:35:24 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,22 @@
+import { stat } from "fs";
+
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {
+        ...state,
+        friends: [...state.friends, action.friend]
+      }
+      break;
+    case 'REMOVE_FRIEND':
+      const index = state.friends.findIndex(friend => friend.id === action.id);
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, index), ...state.friends.slice(index + 1)
+        ]
+      };
+    default:
+      return state;
+  }
 }
