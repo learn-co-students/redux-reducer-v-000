c4205cf186c83bf2d0e55f11770bd9d0f9f8d90e
dwieb <dwayne.harmon@gmail.com>
2018-12-04 01:36:45 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return Object.assign({}, state, {friends: [...state.friends, action.friend]})
+    case 'REMOVE_FRIEND':
+      return { friends: state.friends.filter(item => item.id !== action.id) }
+    default:
+      return state;
+  }
+
 }
+// In manageFriends.js write a function called manageFriends that takes in an action and the previous state as its argument.
+// Here, our action will also have an additional attribute called friend, sometimes an action contains multiple attributes for producing a new state.
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return { numberOfPresents: state.numberOfPresents + 1 }
+    default:
+      return state;
+  }
 }
+//Write a function called managePresents that takes in an action and the previous state as its argument. 
