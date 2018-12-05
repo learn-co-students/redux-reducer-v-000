56741d160f0083106cc06512173add957eab6193
learn <kimfoukaris@outlook.com>
2018-12-05 11:52:26 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return {friends: state.friends.concat({ 
+                name: 'Joe',
+                hometown: 'Boston',
+                id: 101
+              })} 
+        case 'REMOVE_FRIEND':
+            return {friends: state.friends.filter(friend => friend.id !== 101)}
+        default:
+            return state;
+        }
 }
