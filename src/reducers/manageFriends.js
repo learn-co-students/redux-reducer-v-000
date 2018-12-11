e2132485930ae4c27a91256c815350dcf52ccc9b
zep1994 <snaxbags@protonmail.com>
2018-12-11 15:39:14 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
-}
+    switch (action.type) {
+    case 'ADD_FRIEND':
+      return ({
+          ...state, friends: [...state.friends, action.friend]
+        })
+
+    case 'REMOVE_FRIEND':
+
+    const removeFriend = state.friends.findIndex(friends => friends.id === action.id)
+       return (
+           {
+               ...state,
+                friends: [
+                    ...state.friends.slice(0, removeFriend),
+                    ...state.friends.slice(removeFriend + 1)
+                ]
+           }
+       )
+       default:
+       return state;
+   }
+  }
