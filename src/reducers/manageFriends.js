abeb84acf067f8fec5de9a483603e9e8932628f5
asabogal <asabogal.git@gmail.com>
2018-12-22 18:21:27 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
+// let state = {
+//   friends: [
+//     {
+//       name: 'Avi',
+//       hometown: 'NYC',
+//       id: 100
+//     }
+//   ],
+// };
+
+// let action = {
+//    type: "REMOVE_FRIEND",
+//    id: 101
+// }
+
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return (
+        {...state, friends: [...state.friends, action.friend]}
+      )
+    case "REMOVE_FRIEND":
+      return (
+        {...state, friends: state.friends.filter(friend => friend.id !== action.id)}
+      )
+    default:
+        return state
+  }
+
 }
