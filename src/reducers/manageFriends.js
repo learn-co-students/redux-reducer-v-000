643b7d9abe3c8b5d7ed3e56c75b4c9053ff6a0e3
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-02 14:27:39 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,9 @@
 export function manageFriends(state, action) {
   switch (action.type) {
     case 'ADD_FRIEND':
-      return { friends: [...state.friends, action.friend] };
+      return { ...state, friends: [...state.friends, action.friend] };
     case 'REMOVE_FRIEND':
-      return { friends: state.friends.filter(friend => friend.id !== action.id) };
+      return { ...state, friends: state.friends.filter(friend => friend.id !== action.id) };
     default:
       return state;
   }
