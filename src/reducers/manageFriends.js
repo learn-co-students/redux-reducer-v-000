dd59a83a573bd3bf71d1d1d4aa45fa8df2a3c9ef
Micah Shute <micah.shute@gmail.com>
2019-02-05 00:04:01 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return { ...state, friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            const newState = {...state }
+            newState.friends = newState.friends.filter(f => f.id !== action.id )
+            return newState
+        default:
+            return state
+    }
 }
