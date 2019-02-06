690f8b9bf548293f6974f47a77076895ef405eef
github username <github email address>
2019-02-06 16:45:57 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
-export function managePresents(state, action){
+export function managePresents(state = {
+  numberOfPresents: 0
+}, action) {
+  switch (action.type) {
+  	case 'INCREASE':
+  	  return { numberOfPresents: state.numberOfPresents + 1 };
+  	default:
+  	  return state;
+  }
 }
+// managePresents reducer function criteria:
+// returns the existing state if the action's type doesn't match a type in the reducer
+// increases the number of presents if the action's type is 'INCREASE'
