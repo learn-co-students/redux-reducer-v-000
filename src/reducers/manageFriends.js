a3bce85527f086eeece88fdee9429b28e2c83f00
Vibraniumforge <gregm9999@gmail.com>
2019-02-10 12:21:10 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    console.log("state=", state,);
+    console.log("action=", action);
+    console.log('+++++++++++++++++++++++++++=');
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {friends: [...state.friends, action.friend] }
+        case 'REMOVE_FRIEND':
+            const newFriends = [...state.friends].filter((friend) => friend.id !== action.id);
+            return {friends: newFriends }
+        default:
+          return state;
+}
 }
