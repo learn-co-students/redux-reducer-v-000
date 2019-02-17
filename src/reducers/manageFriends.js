a661b08875302acd2cbc8fbfb3ec44bfd3b3ded3
Kachel Wittig <kjwittig@gmail.com>
2019-02-17 17:15:07 -0600

    Donezo

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friend: [] }, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      console.log(action.friend)
+      return {
+        ...state,
+        friends: [...state.friends, action.friend],
+      }
+    case 'REMOVE_FRIEND':
+      console.log(action.id)
+      return {
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.id),
+      }
+    default:
+      return state
+  }
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
