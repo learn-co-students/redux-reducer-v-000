572da189e8234f28814d03a103a1df09ba96b7bd
github username <github email address>
2019-02-23 22:48:33 -0800

    Use spread operator and refactor

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,11 +1,17 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      var newState = state.friends.concat(action.friend);
-      return { friends: newState }
+      return (
+        {...state,
+          friends: [
+            ...state.friends,
+            action.friend
+          ]
+        }
+      )
     case 'REMOVE_FRIEND':
-      var newState = state.friends.filter(friend => friend.id != action.id);
-      return { friends: newState }
+      let filtered = state.friends.filter(friend => friend.id != action.id);
+      return {friends: filtered}
     default:
       return state
   }
