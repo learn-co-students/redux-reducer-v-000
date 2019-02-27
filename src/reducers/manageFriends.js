3321c0c12c3aa636d8738a0c12575f29ce5a4d74
rcrdlbl <bluh@outlook.com>
2019-02-26 21:05:51 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+        return { friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(function(e){return e.id !== action.id}) }
+      break;
+    default:
+      return state
+  }
 }
