ab43019196aee81099d480f25faf740b4dcd620e
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-03-13 18:30:03 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+    debugger;
+    switch(action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: 1}
+        default:
+            return state;
+    }
 }
