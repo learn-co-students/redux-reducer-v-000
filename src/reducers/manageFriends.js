f88d983e6720b35ee52fa26abe9a2e0eda4dc920
Kkelly22 <kk504408@gmail.com>
2019-03-24 11:30:12 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+	switch (action.type) {
+		case 'ADD_FRIEND':
+			 return { ...state, friends: [...state.friends, action.friend]}
+		case 'REMOVE_FRIEND':
+			let newArray = state.friends.slice()
+			let elementPos = newArray.map(function(x) {return x.id; }).indexOf(action.id);
+  			newArray.splice(elementPos, 1)
+  			return {...state, friends: newArray}
+		default:
+			return state
+	}
 }
