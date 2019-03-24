c71273e76e8817aa7fe71be3fee2b72191c734f6
Kkelly22 <kk504408@gmail.com>
2019-03-24 11:31:38 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,10 +3,19 @@ export function manageFriends(state, action){
 		case 'ADD_FRIEND':
 			 return { ...state, friends: [...state.friends, action.friend]}
 		case 'REMOVE_FRIEND':
-			let newArray = state.friends.slice()
-			let elementPos = newArray.map(function(x) {return x.id; }).indexOf(action.id);
-  			newArray.splice(elementPos, 1)
-  			return {...state, friends: newArray}
+			// let newArray = state.friends.slice()
+			// let elementPos = newArray.map(function(x) {return x.id; }).indexOf(action.id);
+  	// 		newArray.splice(elementPos, 1)
+  	// 		return {...state, friends: newArray}
+		  	const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+		      return (
+		        {...state,
+		            friends: [
+		              ...state.friends.slice(0, removalIndex),
+		              ...state.friends.slice(removalIndex + 1)
+		            ]
+		        }
+		      )
 		default:
 			return state
 	}
