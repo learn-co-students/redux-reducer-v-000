0811c73fe5c5a56a5eecb553ed316c3b345054e7
ElleeB <laura.trager@gmail.com>
2019-03-25 10:55:35 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return (
+        // return a new object with key-value pairs from state with the
+        // friends: key overwritten with the current friends values, plus the addition of friend
+        {...state, friends: [...state.friends, action.friend]}
+      )
+    case 'REMOVE_FRIEND':
+    // the action has a property of the friends id to be removed
+      const removeAtIndex = state.friends.findIndex(friend => friend.id === action.id)
+
+      return (
+        {...state,
+          friends: [
+            // return elements prior to removalIndex
+            ...state.friends.slice(0, removeAtIndex),
+            // return elements after removeAtIndex
+            ...state.friends.slice(removeAtIndex + 1)
+          ]
+        }
+      )
+
+    default:
+      return state
+  }
 }
