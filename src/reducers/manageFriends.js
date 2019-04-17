00e2933ca08ed874820a033b7643ac80bab2ea16
Mark Lawrence <lawrence.ml@gmail.com>
2019-04-16 21:56:48 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { ...state, friends: [...state.friends, action.friend] };
+
+    case "REMOVE_FRIEND":
+      const arrayPosition = state.friends.findIndex(
+        friend => action.id === friend.id
+      );
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, arrayPosition),
+          ...state.friends.slice(arrayPosition + 1)
+        ]
+      };
+
+    default:
+      return state;
+  }
 }
