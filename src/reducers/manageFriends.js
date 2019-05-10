3a86a3acfb8e72ffdcc266565570eeca972f8f11
Jayon Kim <jpkim921@gmail.com>
2019-05-10 00:09:38 -0400

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends:[...state.friends, action.friend]}
+      case "REMOVE_FRIEND":
+        return {friends: state.friends.filter(friend => friend.id != action.id)}
+    default:
+      return state;
+  }
 }
+
+// let state = {
+//   friends: [
+//     {
+//       name: 'Avi',
+//       hometown: 'NYC',
+//       id: 100
+//     }
+//   ],
+// };
