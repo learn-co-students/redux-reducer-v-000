49ba7caed94f3a8530be56dc95d42b0e2fb1ac63
roitblatari <roitblatari@gmail.com>
2019-06-12 11:51:08 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+	// console.log(state);
+
+	switch (action.type) {
+		case 'ADD_FRIEND':
+			return {
+				friends: [ ...state.friends, action.friend ]
+			};
+		case 'REMOVE_FRIEND':
+			// console.log('in the switch/remove : ', action);
+			return {
+				friends: [ ...state.friends.filter((friend) => friend.id !== action.id) ]
+			};
+		default:
+			return state;
+	}
 }
