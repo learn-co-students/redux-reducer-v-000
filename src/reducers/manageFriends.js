d38d343a661ac8448ce58a32adc0702350b680d0
Lara Sanders <larasanders@gmail.com>
2019-06-16 17:10:35 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
+  let state = {manageFriends: 0}
+  let friends = {name: 'Avi', hometown: 'NYC', id: 100}
+  let friend = {name: 'Joe', hometown: 'Boston', id: 101}
+
 export function manageFriends(state, action){
-}
+
+    switch (action.type) {
+    case 'ADD_FRIEND':
+      return manageFriends, {friends: [friends, friend]}
+      default:
+        return state;
+        case "REMOVE_FRIEND":
+        const removeFriend = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+          {...state,
+              friends: [
+                ...state.friends.slice(0,removeFriend),
+                ...state.friends.slice(removeFriend + 1)
+              ]
+          }
+        )
+
+    }
+  };
