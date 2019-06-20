9eb8e661ef96ddf09273ab19b7916b8e67c16c7f
github username <github email address>
2019-06-20 09:27:48 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,46 @@
 export function manageFriends(state, action){
+
+  if (action.type === 'ADD_FRIEND' && Object.keys(action.friend).includes('name' && 'hometown' && 'id')) {
+    return { friends: [...state.friends, action.friend] }
+  } else if (action.type === 'REMOVE_FRIEND' && Object.keys(action).includes('id')) {
+    const indexRemove = state.friends.findIndex(element => element.id === action.id)
+    return { friends: state.friends.filter((el, index) => index !== indexRemove)}
+  }
+    else {
+    return state
+  }
 }
+
+
+// let state = {
+//   friends: [
+//     {
+//       name: 'Avi',
+//       hometown: 'NYC',
+//       id: 100
+//     }
+//   ],
+// };
+//
+// {
+//   type: "ADD_FRIEND",
+//   friend: {
+//     name: 'Joe',
+//     hometown: 'Boston',
+//     id: 101
+//   }
+
+// {
+//   friends: [
+//     {
+//       name: 'Avi',
+//       hometown: 'NYC',
+//       id: 100
+//     },
+//     {
+//       name: 'Joe',
+//       hometown: 'Boston',
+//       id: 101
+//     }
+//   ]
+// }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+  // if (action.type === 'INCREASE') {
+  //   return { numberOfPresents: state.numberOfPresents + 1 }
+  // }
+  switch (action.type) {
+    case 'INCREASE' :
+      return { numberOfPresents: state.numberOfPresents + 1 }
+    default :
+      return state
+  }
 }
