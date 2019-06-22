50dbd8d09a006d14a631b96af14297ef4f6c4de1
BrentBauman86 <brentbauman86@gmail.com>
2019-06-22 06:04:23 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
-}
+    switch (action.type) {
+      case 'ADD_FRIEND':
+        if (action.friend.name && action.friend.hometown && action.friend.id) {
+          let newFriends = [...state.friends, action.friend]
+          return {friends: newFriends}
+        } else {
+          return state;
+        }
+      case 'REMOVE_FRIEND':
+        if (action.id) {
+          let removedFriend = state.friends.findIndex((friend) => friend.id === action.id)
+          let updatedFriends = [...state.friends.slice(0, removedFriend), ...state.friends.slice(removedFriend + 1)]
+          return {friends: updatedFriends}
+        } else {
+          return state
+        }
+      default:
+        return state;
+     }
+  }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
-export function managePresents(state, action){
+let action = {
+    type: "INCREASE"
 }
+
+export function managePresents(state, action){
+    switch (action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: state.numberOfPresents + 1}
+        case 'Random Action Type':
+            return state; 
+    }_
+}
+
