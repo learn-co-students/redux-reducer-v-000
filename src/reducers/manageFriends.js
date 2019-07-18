6edccd343f9f11e25a8144b45657473e548ef319
sammy-brake <sammy.brake@gmail.com>
2019-07-18 08:10:42 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,13 +1,11 @@
 
-export function manageFriends(state, action){
-    switch(action.type){
-        case "ADD_FRIEND":
-            return { ...state, friends: [...state.friends, action.friend]}
-        case "REMOVE_FRIEND":
-            const newState = {...state }
-            newState.friends = newState.friends.filter(f => f.id !== action.id )
-            return newState
-        default:
-            return state
-    }
-}
+// export function manageFriends(state, action){
+//     switch(action.type){
+//       case 'ADD_FRIEND':
+//       return {...state, friends: state.friends.concat(action.friend)}
+//       case 'REMOVE_FRIEND':
+//       return {...state, friends: state.friends.filter(friend => friend.id !== action.id)}
+//       default:
+//       return state
+//     }
+//   }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,8 @@
-export function managePresents(state, action){
-    switch(action.type){
-        case "INCREASE":
-            return { ...state, numberOfPresents: state.numberOfPresents + 1 }
-        default:
-            return state
-    }
-}
+// export function managePresents(state, action){
+//     switch(action.type){
+//       case 'INCREASE':
+//       return {...state, numberOfPresents: state.numberOfPresents + 1}
+//       default:
+//       return state
+//     }
+//   }
