085659cc35c10fe777b11fd3a0f0e2415e34fc9c
Rose Brennan <rose@grubhub.com>
2019-07-24 13:58:24 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return (
+        {...state,
+          friends: [
+            ...state.friends,
+            action.friend
+          ]
+        }
+      )
+      
+    case 'REMOVE_FRIEND':
+      const unfriend = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+          friends: [
+            ...state.friends.slice(0, unfriend),
+            ...state.friends.slice(unfriend + 1)
+          ]
+        }
+      )
+
+    default:
+      return state;
+  }
 }
