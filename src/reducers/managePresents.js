9f97c6736cd29bd2e46404b53e4bc019103e80cd
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-16 16:39:32 -0400

    One test passing

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,18 @@
 export function managePresents(state, action){
+
+  state = {
+    numberOfPresents: 0
+  }
+
+  managePresents = (state, action) => {
+    // if (typeof action !== 'INCREASE') {
+    //   return state
+    // }
+    switch (action.type) {
+      case 'INCREASE':
+        return {numberOfPresents: state.numberOfPresents + 1}
+      default:
+        return state
+    }
+  }
 }
