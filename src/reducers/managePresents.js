c964cdd0b3ec05a0ff58557ef65dcf7fd64a08ca
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-16 16:49:12 -0400

    managePresents - working

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,18 +1,8 @@
 export function managePresents(state, action){
-
-  state = {
-    numberOfPresents: 0
-  }
-
-  managePresents = (state, action) => {
-    // if (typeof action !== 'INCREASE') {
-    //   return state
-    // }
-    switch (action.type) {
-      case 'INCREASE':
-        return {numberOfPresents: state.numberOfPresents + 1}
-      default:
-        return state
-    }
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state
   }
 }
