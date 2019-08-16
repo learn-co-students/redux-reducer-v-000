5a5939008264b2a13d4eeb03ed8a5c03048c1740
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-16 18:07:16 -0400

    wip- need to push inside of an array

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      // return Object.assign({}, state, {
+      //   friend: {
+      //     name: action.friend.name,
+      //     hometown: action.friend.hometown,
+      //     id: action.friend.id
+      //   }
+              
+    return {...state,
+      friend: {
+        name: action.friend.name,
+        hometown: action.friend.hometown,
+        id: action.friend.id
+      }
+    }
+      // return {...action.friend,
+      //   name: state.friend.name,
+      //   hometown: state.friend.hometown,
+      //   id: state.friend.id + 1
+      // }
+    // case 'REMOVE_FRIEND':
+    default:
+          return state  
+  }     
 }
+
