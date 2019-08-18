15e9eee1230fa71930f1abb2b070de6665814cb0
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-18 09:27:35 -0400

    One test not passing

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,28 +1,17 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      // return Object.assign({}, state, {
-      //   friend: {
-      //     name: action.friend.name,
-      //     hometown: action.friend.hometown,
-      //     id: action.friend.id
-      //   }
-              
-    return {...state,
-      friend: {
-        name: action.friend.name,
-        hometown: action.friend.hometown,
-        id: action.friend.id
+      return {
+        ...state,
+        friends: state.friends.concat(action.friend)
       }
-    }
-      // return {...action.friend,
-      //   name: state.friend.name,
-      //   hometown: state.friend.hometown,
-      //   id: state.friend.id + 1
-      // }
-    // case 'REMOVE_FRIEND':
+    
+    case 'REMOVE_FRIEND':
+      return {
+        ...state,
+        friends: state.friends.filter(({ id }) => id !== action.data)
+      }
     default:
           return state  
   }     
 }
-
