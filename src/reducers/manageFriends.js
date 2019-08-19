bf1865eb04eeea1c91a6e05c3708c66cda6911c5
Ashley <ashleywong678@gmail.com>
2019-08-19 15:50:06 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,5 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
   if(action.type === 'ADD_FRIEND'){
-    return {friends: state.friends << action.friend }
+    return {...state, friends: [...state.friends, action.friend] }
+  } else if(action.type === 'REMOVE_FRIEND'){
+    const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+  } else {
+    return {friends: state.friends }
   }
 }
