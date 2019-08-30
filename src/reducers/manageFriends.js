b4b9b022600aa07b8b01d5086d3ddf12ccb977eb
Christopher DiStasio <cadistasio@gmail.com>
2019-08-30 08:25:54 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  action = {
+    type: "ADD_FRIEND",
+    friend: "Chrome Boi"
+  }
 }
+
+// function reducer(state, action){      
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     case 'DECREASE_COUNT':
+//       return {count: state.count - 1}
+//     default:
+//       return state;
+//   }
+// }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,30 @@
+let state = {presents: []}
+
+let action = {type: 'ADD_PRESENTS'}
+
 export function managePresents(state, action){
+  
+  switch (action.type) {
+    case 'ADD_PRESENTS':
+      return {presents};
+    default: 
+    return state
+  }
+  
+
 }
+
+// function reducer(state, action){      
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     case 'DECREASE_COUNT':
+//       return {count: state.count - 1}
+//     default:
+//       return state;
+//   }
+// } 
+
+// action = {
+  //   type: "INCREASE"
+  // }
