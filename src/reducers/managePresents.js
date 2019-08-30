42618fb66c2585a0b5bbdfc204a0bf4a000fc4b5
Christopher DiStasio <cadistasio@gmail.com>
2019-08-30 14:50:22 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,30 +1,16 @@
-let state = {presents: []}
+export function managePresents(
+  state = {
+    numberOfPresents: 0
+  }, action ){
 
-let action = {type: 'ADD_PRESENTS'}
-
-export function managePresents(state, action){
-  
   switch (action.type) {
-    case 'ADD_PRESENTS':
-      return {presents};
+    case 'INCREASE':
+      return Object.assign(
+      {}, state, {
+        numberOfPresents: state.numberOfPresents + 1 
+      })
+
     default: 
-    return state
+      return state
   }
-  
-
 }
-
-// function reducer(state, action){      
-//   switch (action.type) {
-//     case 'INCREASE_COUNT':
-//       return {count: state.count + 1}
-//     case 'DECREASE_COUNT':
-//       return {count: state.count - 1}
-//     default:
-//       return state;
-//   }
-// } 
-
-// action = {
-  //   type: "INCREASE"
-  // }
