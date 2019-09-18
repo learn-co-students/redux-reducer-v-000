8d061310fa5461faf533c1aa38b2e94908e26cdc
christianavdm <christianavdm@gmail.com>
2019-09-18 12:12:34 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,13 +1,13 @@
 export function manageFriends(state, action){
+    let new_friends = state.friends.slice(0);   
     switch (action.type) {
         case 'ADD_FRIEND':
-          let new_friends = state.friends; 
           new_friends.push(action.friend); 
           return {friends: new_friends}; 
         case 'REMOVE_FRIEND':
-            let new_friends = state.friends; 
-          //  const index = new_friends.findIndex(friend => element.index === action.index); 
-          return new_friends; 
+            const friendIndex = new_friends.findIndex(friend => friend.id === action.id); 
+            new_friends.splice(friendIndex, 1);
+           return {friends: new_friends}; 
         default:
           return state;
       }
