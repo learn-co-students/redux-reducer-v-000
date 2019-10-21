6c396e9f90639c2b70ce1c68092be3818cf264f2
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-10-21 16:38:37 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
+let state = {
+  friends: []
+}
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+    console.log(...state)
+    
+    case 'REMOVE_FRIEND':
+    console.log(...state)
+    default:
+      return state;
+  }
 }
