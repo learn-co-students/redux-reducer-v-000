d2ef351a66947e441e535617cdbe2da950cd9c38
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-11-25 23:58:09 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
+let state = {friends: []}
+
 export function manageFriends(state, action){
+	switch (action.type) {
+    case 'ADD_FRIEND':
+      const newFriend = {name: action.friend.name,
+          hometown: action.friend.hometown,
+          id: action.friend.id}
+      const newState = {...state}
+      newState.friends.push(newFriend)
+      return newState
+    case 'REMOVE_FRIEND':
+      
+    default:
+      return state;
+  }
 }
+
+/*
+
+state = [{
+	id: 1,
+
+}];
+
+
+
+*/
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
+let state = {numberOfPresents: 0}
+
+//send data from application to store
+
 export function managePresents(state, action){
+	switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
