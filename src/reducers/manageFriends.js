9fcd42999698c3d24a3efd8c5fb7805e4ad8bb16
SiamKing <thesiameseelectric@gmail.com>
2020-01-03 09:39:53 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+    let friends = state.friends
+    switch (action.type) {
+        case ("ADD_FRIEND"):
+            let newFriends = { "friends": [...state.friends, action.friend] }
+            return newFriends;
+        case ("REMOVE_FRIEND"):
+            let removedFriends = { "friends": state.friends.filter((friend) => friend.id !== action.id) }
+            return removedFriends;
+        default:
+            return state;
+    }
 }
