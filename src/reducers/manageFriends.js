76de32da94767daa8e281d8731bb0cf398083002
Jason Mei <jason.mei403@gmail.com>
2020-02-05 22:15:39 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      let newState = {...state}
+      newState.friends.push(action.friend)
+      return { friends: newState.friends }
+    case 'REMOVE_FRIEND':
+      let copy = {...state}
+      let target = copy.friends.find(friend => friend.id === action.id)
+      let targetIndex = copy.friends.indexOf(target)
+      copy.friends.splice(targetIndex, 1)
+      return {friends: copy.friends}
+    default:
+      return state
+  }
 }
