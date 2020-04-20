077e3be6a2bf5abf9299a11a9b6b408794c281da
Rebecca Tong <rtong@buffalo.edu>
2020-04-20 16:15:25 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,22 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+
+switch(action.type){
+    case 'ADD_FRIEND':
+    return {
+      friends:[ ...state.friends, {
+      hometown: action.friend.hometown,
+      id: action.friend.id,
+      name: action.friend.name
+    } ]
+  }
+      case 'REMOVE_FRIEND':
+      return{
+        friends: state.friends.filter(function(value){
+          return action.id != value.id
+          })
+        }
+    default:
+    return state;
+  }
+
 }
