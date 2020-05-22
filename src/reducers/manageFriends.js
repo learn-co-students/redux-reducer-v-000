e4fc7b070dbc1651404c67dead00cc396579f0cf
Dalia <dalia.sawaya@flatironschool.com>
2020-05-22 17:24:48 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,24 +4,26 @@ export function manageFriends(state = {
   switch(action.type) {
 
     case "ADD_FRIEND":
-      return Object.assign({}, state, {
+      return {...state,
         friends: [
           ...state.friends,
           action.friend
         ]
-      });
+      };
 
     case "REMOVE_FRIEND":
       const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
-      return Object.assign({}, state, {
+      return {...state,
         friends: [
           ...state.friends.slice(0, removalIndex),
           ...state.friends.slice(removalIndex + 1)
         ]
-      });
+      };
 
     default:
       return state;
 
   }
 };
+
+//see managePresents.js for notes on spread vs Object.assign()
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -5,9 +5,18 @@ export function managePresents(state = {
   switch(action.type) {
 
     case "INCREASE":
-      return Object.assign({}, state, {
+      return {...state,
         numberOfPresents: state.numberOfPresents + 1
-      });
+      };
+      //"Return a new object that contains all the key-value pairs 
+      //from dog copied over with the age key overwritten with a new value"
+
+      //same:
+      // return Object.assign({}, state, {
+      //   numberOfPresents: state.numberOfPresents + 1
+      // });
+      //"Start with a new empty object, 
+      //copy over everything from the original state, then overwrite the numberofpresents property with a new value."
 
     default:
       return state;
