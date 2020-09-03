207a248ce2c363d61c01dfcf1b5715efb4d8533e
jamieberrier <berrier.jamie@gmail.com>
2020-09-03 10:34:26 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    // the initial state should be an object with a key, friends, set to an empty array.
+    // handle two actions, "ADD_FRIEND" and "REMOVE_FRIEND". 
+    switch (action.type) {
+        // When adding a friend, the action will include a friend key assigned to an object with name, hometown, and id keys.
+        case 'ADD_FRIEND':
+            // return a new state with this friend object added to the friends array.
+            return {friends: state.friends.concat(action.friend)}
+        case 'REMOVE_FRIEND':
+            return {friends: state.friends.filter(friend => friend.id != action.id)}
+        default:
+            return state
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+    // Set an default value for the state argument - an object with a key, numberOfPresents, assigned to 0.
+    switch (action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: state.numberOfPresents + 1}
+    
+        default:
+            return {numberOfPresents: 0}
+    }
 }
