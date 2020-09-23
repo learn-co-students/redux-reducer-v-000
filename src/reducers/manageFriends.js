044f8b6e2bea3f6804e76df1c87db36f6b6ca372
Olivia Tufte <ortufte@gmail.com>
2020-09-23 13:46:57 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND' :
+            return { ...state, 
+                friends: [...state.friends, action.friend] 
+            }
+        case 'REMOVE_FRIEND' :
+            return { ...state, 
+                friends: state.friends.filter(friend => friend.id != state[action.id])}
+        default: 
+            return state;
+    }
 }
+
+
