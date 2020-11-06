79e33b9983f6531529290a8cda14915f12bc0b9c
David <davidlago104@gmail.com>
2020-11-06 14:55:05 -0500

    Done

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+    let numberOfPresents = 0
+    
+    if (action.type === 'INCREASE'){
+        return {numberOfPresents: state.numberOfPresents + 1}
+    }
+    else {
+        return {numberOfPresents}
+    }
+    
 }
