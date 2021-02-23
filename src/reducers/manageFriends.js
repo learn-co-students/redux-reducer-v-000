29d00fedd0126e58c2bc79966b19a0998e7c0583
RonSala <ron.sala@gmail.com>
2021-02-23 17:22:49 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,34 +3,18 @@ export function manageFriends(state, action){
     case 'ADD_FRIEND':
       const addState = Object.assign({}, state);
       addState.friends.push(action.friend);
-      // console.log('state:', state);
-      // console.log('addState:', addState);
-      
       return addState;
     case 'REMOVE_FRIEND':
-      console.log('state before:', state)
-
-      const isId = (element) => element.id == action.id;
+      const isId = (element) => element.id === action.id;
       const index = state.friends.findIndex(isId);
-      
       const friendsAry = [];
       state.friends.map((friend) => {
         friendsAry.push(friend)
       });
-
-      friendsAry.splice(index, 1); 
-      console.log('state after:', state)
-
-      // rmState = Object.assign(rmState, state);
-      // console.log('rmState before:', rmState);
-      // rmState.friends.splice(index, 1);
-
-      // return rmState;
- 
+      friendsAry.splice(index, 1);
       const newState = Object.assign({}, state, {
         friends: friendsAry
       });
-      // console.log('newState after:', newState);
       return newState;
     default:
       return state;
