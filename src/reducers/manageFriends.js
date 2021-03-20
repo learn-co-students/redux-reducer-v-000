fe6578c6aa298947ebc61c56f16bdd11e71832d2
ricecake88 <gracelearnsforever@gmail.com>
2021-03-20 13:19:39 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+    console.log(state.friends);
+    console.log(action.id);
+    switch(action.type) {
+        case "ADD_FRIEND": 
+            return {
+                ...state,
+                friends: [...state.friends, {
+                    hometown: action.friend.hometown,
+                    id:action.friend.id,
+                    name:action.friend.name,
+                }]
+            }
+        case "REMOVE_FRIEND":
+              let newFriends = state.friends.filter(friend => 
+                    {return friend.id !== action.id})
+              return {friends: newFriends};
+            break;
+        default:
+            return {friends: state.friends};
+    }
+
 }
