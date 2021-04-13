7f500f86b4fca8667929aab6c9a2ccaed4945ab9
Damien de Lartigue <damien@delartigue.co.uk>
2021-04-13 13:32:02 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+  
+  switch(action.type) {
+    case "ADD_FRIEND":
+      const currentFriends = state.friends
+      currentFriends.push(action.friend)
+      return {friends: currentFriends}
+      break
+    case "REMOVE_FRIEND":
+      const newFriends = state.friends.filter(friend => friend.id != action.id)
+      return {friends: newFriends}
+      break
+    default:
+      return state
+      break
+  }
+  
 }
