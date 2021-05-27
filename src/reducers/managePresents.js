b0635aedb8961d371f39598e456ca3dff1607fe1
guysbryant <guy.s.bryant@gmail.com>
2021-05-27 00:00:44 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,17 @@
-export function managePresents(state, action){
+export function managePresents(state = { numberOfPresents: 0 }, action){
+  return {...state, numberOfPresents: action.type === "INCREASE" ? state.numberOfPresents + 1 : state.numberOfPresents}
+
+  // if (action.type === "INCREASE"){
+  //   return { numberOfPresents: state.numberOfPresents + 1 }
+  // } else { return state }
+
+  // switch(action.type){
+  // case "INCREASE":
+  //   return {
+  //     ...state,
+  //     numberOfPresents: state.numberOfPresents + 1
+  //   }
+  // default: return state
+  // }
 }
+
