1856f4f162d058d1f836fe687d7dd010aafb20ff
Tom Tobar <tomas.tobar@flatironschool.com>
2021-06-18 19:27:31 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {
+                friends: [...state.friends, action.friend]
+            }
+        case "REMOVE_FRIEND":
+            const removed = state.friends.find(friend => friend.id === action.id)
+            const indexOfRemoved = state.friends.indexOf(removed);
+            const friendsCopy = [...state.friends]
+            friendsCopy.splice(indexOfRemoved, 1)
+            return {
+                    friends: friendsCopy
+                }
+        default:
+            return state;
+    }
 }
